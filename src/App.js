206dd1fdca8fee8ffc3f4c206f854a90ba03d81b
sezenyildirim <bestesyildirim@gmail.com>
2023-12-25 15:20:42 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" exact activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/users" activeClassName="active">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+export default App;
